003dff90ca7b224e92ed87c3bb05a0f2b22ab204
RaMiyahira <ramiyahira@minha.fag.edu.br>
2025-04-25 19:59:11 -0300

    Add files via upload

---
+++ Alunos/prova1tri/src/prova1tri/Show.java
@@ -0,0 +1,17 @@
+package prova1tri;
+
+public class Show extends Evento {
+    public Show(String nome, String dataInicio, String dataFim, double valorDiaria, int vagasMaximas) {
+        super(nome, dataInicio, dataFim, valorDiaria, vagasMaximas);
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        System.out.println("Evento: " + nome);
+        System.out.println("Tipo: Congresso");
+        System.out.println("Data Início: " + dataInicio);
+        System.out.println("Data Fim: " + dataFim);
+        System.out.println("Valor Diária: R$ " + valorDiaria);
+        System.out.println("Vagas Máximas: " + vagasMaximas);
+    }
+}
