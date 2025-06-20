1e86c6234273b5233a6ac717a89c48454842bb81
Kauê Correia de Oliveira <163582374+kaueCorreia@users.noreply.github.com>
2025-06-20 13:50:15 -0300

    aula 12
    atividade sobre o clime

---
+++ Alunos/KauêCorreiadeOliveira/Aula-12/ClimaObj.java
@@ -0,0 +1,19 @@
+public class ClimaObj {
+    String temperaturaAtual;
+    String temperaturaMaxima;
+    String temperaturaMinima;
+    String umidade;
+    String condicao;
+    String precipitacao;
+    String vento;
+
+    void exibir() {
+        System.out.println("Temperatura atual: " + temperaturaAtual + "°C");
+        System.out.println("Temperatura máxima: " + temperaturaMaxima + "°C");
+        System.out.println("Temperatura mínima: " + temperaturaMinima + "°C");
+        System.out.println("Umidade: " + umidade + "%");
+        System.out.println("Condição do tempo: " + condicao);
+        System.out.println("Precipitação: " + precipitacao + " mm");
+        System.out.println("Vento: " + vento + " km/h");
+    }
+}
