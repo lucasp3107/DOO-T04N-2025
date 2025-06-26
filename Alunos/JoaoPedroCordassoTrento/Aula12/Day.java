c7b7dfca81576c2bb995e1a4ac5b513485b71e77
JoaoTrento <168440911+JoaoTrento@users.noreply.github.com>
2025-06-26 17:56:52 -0300

    Aula 12

---
+++ Alunos/JoaoPedroCordassoTrento/Aula12/Day.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+class Day {
+    public String datetime;
+    public long datetimeEpoch;
+    public double tempmax;
+    public double tempmin;
+    public double temp;
+    public double feelslikemax;
+    public double feelslikemin;
+    public double feelslike;
+    public double dew;
+    public double humidity;
+    public double precip;
+    public double precipprob;
+    public double precipcover;
+    public List<String> preciptype;
+    public double snow;
+    public double snowdepth;
+    public double windgust;
+    public double windspeed;
+    public double winddir;
+    public double pressure;
+    public double cloudcover;
+    public double visibility;
+    public double solarradiation;
+    public double solarenergy;
+    public int uvindex;
+    public int severerisk;
+    public String sunrise;
+    public long sunriseEpoch;
+    public String sunset;
+    public long sunsetEpoch;
+    public double moonphase;
+    public String conditions;
+    public String description;
+    public String icon;
+    public List<String> stations;
+    public String source;
+    public List<Hour> hours;
+}
