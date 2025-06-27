f9cccb6c9b28237a835f104e9eec328b930b8676
Gabriel Eduardo Philippsen Wandscheer <159468605+Gabehepw@users.noreply.github.com>
2025-06-27 19:29:58 -0300

    Create Classificacao.java

---
+++ Alunos/GabrielEPW/Prova_2tri/Classificacao.java
@@ -0,0 +1,70 @@
+import java.util.List;
+
+public class Classificacao {
+    private int id;
+    private String nome;
+    private String idioma;
+    private List<String> generos;
+    private double nota;
+    private String status;
+    private String dataEstreia;
+    private String dataFim;
+    private String emissora;
+
+    public Serie() {}
+
+    public Serie(int id, String nome, String idioma, List<String> generos, double nota, String status,
+                 String dataEstreia, String dataFim, String emissora) {
+        this.id = id;
+        this.nome = nome;
+        this.idioma = idioma;
+        this.generos = generos;
+        this.nota = nota;
+        this.status = status;
+        this.dataEstreia = dataEstreia;
+        this.dataFim = dataFim;
+        this.emissora = emissora;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public List<String> getGeneros() {
+        return generos;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDataEstreia() {
+        return dataEstreia;
+    }
+
+    public String getDataFim() {
+        return dataFim;
+    }
+
+    public String getEmissora() {
+        return emissora;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %s\nIdioma: %s\nGêneros: %s\nNota: %.1f\nStatus: %s\nEstreia: %s\nFim: %s\nEmissora: %s",
+            nome, idioma, generos, nota, status, dataEstreia, dataFim, emissora);
+    }
+}
