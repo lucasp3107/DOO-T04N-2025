282a4675acc1d09283059ccea84b469f5f697c33
Gustavo pellá Bazanella <150949496+GustavoPella@users.noreply.github.com>
2025-07-03 22:34:27 -0300

    fixes

---
+++ Alunos/Gustavo Pellá Bazanella/prova_2tri/service/TvMazeService.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.Serie;
+
+public class TvMazeService {
+    public Serie buscarSeriePorNome(String nome) {
+        // TODO: Implementar chamada HTTP para a API do TVMaze
+        return null;
+    }
+}
